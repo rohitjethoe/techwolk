d198daab50ba1d679e5a13ef792768e4de2323ac
rohitjethoe <rohitjethoe@proton.me>
2023-05-02 20:09:08 +0200

    margins.

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,7 +1,7 @@
 <template>
     <Layout>
         <div v-if="post">
-            <div class="w-11/12 mx-auto my-2 sm:my-12">
+            <div class="w-11/12 mx-auto my-2 sm:mt-12">
                 <div class="text-sm">
                     <router-link class="hover:border-black hover:border-b-2" to="/">Home</router-link> / Artikel
                 </div>
